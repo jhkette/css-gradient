55352135bb721639c701e2d77096559d93c24347
khenryjoe <joseph.ketterer@gmail.com>
2020-09-28 12:42:28 +0100

    css

--- src/App.vue
+++ src/App.vue
@@ -62,10 +62,11 @@ h1 {
   width: 80%;
   margin: auto;
   position: relative;
-  top: -9.5rem;
+  top: -10rem;
   background-color: #eaeaea;
   border-radius: 1rem;
   padding: 2rem; 
+  border: 2px solid #24292e;
 
 }
 .heading{
